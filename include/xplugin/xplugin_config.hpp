d981a7889d182256d52110ce7f09249eeb1a0e0a
Johan Mabille <johan.mabille@gmail.com>
2018-10-30 15:54:58 +0100

    gitignore, first include and CMakeLists.txt

---
+++ include/xplugin/xplugin_config.hpp
@@ -0,0 +1,29 @@
+/***************************************************************************
+* Copyright (c) 2018, Johan Mabille, Sylvain Corlay, Wolf Vollprecht and   *
+* Martin Renou                                                             *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#ifndef XPLUGIN_CONFIG_HPP
+#define XPLUGIN_CONFIG_HPP
+
+#ifdef _WIN32
+    #ifdef XPLUGIN_EXPORTS
+        #define XPLUGIN_API __declspec(dllexport)
+    #else
+        #define XPLUGIN_API __declspec(dllimport)
+    #endif
+#else
+    #define XPLUGIN_API
+#endif
+
+// Project version
+#define XPLUGIN_VERSION_MAJOR 0
+#define XPLUGIN_VERSION_MINOR 0
+#define XPLUGIN_VERSION_PATCH 0
+
+#endif
+
