50939effa7c20d012093c72a7ca505056e5c7fe5
DerThorsten <derthorstenbeier@gmail.com>
2023-08-01 11:01:14 +0200

    removal of flags

--- cmake/xplugin_emscripten_utils.cmake
+++ cmake/xplugin_emscripten_utils.cmake
@@ -1,3 +1,11 @@
+function(xplugin_add_emscripten_main_module_flags target_name)
+    if(EMSCRIPTEN)
+        target_link_options(${target_name}
+            PUBLIC "SHELL: -sMAIN_MODULE=1"
+            PUBLIC "SHELL: -sMODULARIZE=1"
+        )
+    endif()
+endfunction()
 
 function(xplugin_add_emscripten_flags)
     if(EMSCRIPTEN)
@@ -5,15 +13,5 @@ function(xplugin_add_emscripten_flags)
         set(CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS "-s SIDE_MODULE=1")
         set(CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS "-s SIDE_MODULE=1")
         set(CMAKE_STRIP FALSE)
-    endif()
-endfunction()
-
-
-function(xplugin_add_emscripten_main_module_flags target_name)
-    if(EMSCRIPTEN)
-        target_link_options(${target_name}
-            PUBLIC "SHELL: -sMAIN_MODULE=1"
-            PUBLIC "SHELL: -sMODULARIZE=1"
-        )
     endif()
 endfunction()
